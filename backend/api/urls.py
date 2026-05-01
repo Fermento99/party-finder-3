5c119101f8e05a85748ea140aedada3e36c0e0c8
Fermento99 <pk.pawlak99@gmail.com>
2026-05-01 16:33:05 +0200

    add auth endpoints

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from .views import BandListCreate, FestivalListCreate, FestivalDetails
+from .views import (
+    BandListCreate,
+    FestivalListCreate,
+    FestivalDetails,
+    RegisterUser,
+    LoginUser,
+    logout_user,
+)
 
 urlpatterns = [
     path("bands/", BandListCreate.as_view()),
     path("festivals/", FestivalListCreate.as_view()),
     path("festivals/<int:id>/", FestivalDetails.as_view()),
+    path("auth/register/", RegisterUser.as_view()),
+    path("auth/login/", LoginUser.as_view()),
+    path("auth/logout/", logout_user),
 ]
